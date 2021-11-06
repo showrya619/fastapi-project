ac310dfb9bfadf54ebcfdf5e21c0f86cd7526572
showrya619 <showrya619@gmail.com>
2021-11-06 12:27:25 +0530

    pydanic schema

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
+
+
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 app = FastAPI()
 
 
@@ -18,7 +31,19 @@ async def get_posts():
     return {"data": "This is your posts"}
 
 
-@app.post("/createposts")
+@app.post("/createpostsv1")
+#Without pydantic
 async def create_posts(payload: dict = Body(...)):
     print(payload)
     return {"new_post": f"title: {payload['title']} content: {payload['content']}"   }
+
+
+@app.post("/createpostsv2")
+#With pydantic
+async def create_postsv2(post: Post):
+    #print(new_post.dict())
+    print(post.title)
+    print(post.content)
+    print(post.published)
+    print(post.rating)
+    return {"data":post.dict()}
